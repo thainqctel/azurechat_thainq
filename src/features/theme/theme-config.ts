acf60d4645736d959195c29562e6c4620964af53
ThaiNQ <thai.nq@cmctelecom.vn>
2024-11-13 09:55:27 +0700

    change theme config

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
-export const AI_NAME = "Azure Chat";
-export const AI_DESCRIPTION = "Azure Chat is a friendly AI assistant.";
+export const AI_NAME = "VLU Chat Assistant";
+export const AI_DESCRIPTION = "GenAI chat tra cứu thông tin.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
